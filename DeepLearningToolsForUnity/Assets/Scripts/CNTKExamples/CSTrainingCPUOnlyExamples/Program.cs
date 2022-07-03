6070f0296a38fc32dc21799ff3eb0a6420f59bba
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-07-03 16:29:51 +0000

    Change.

--- DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/CSTrainingCPUOnlyExamples/Program.cs
+++ DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/CSTrainingCPUOnlyExamples/Program.cs
@@ -5,4 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CN
+namespace CNTK.CSTrainingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = DeviceDescriptor.CPUDevice;
+            Console.WriteLine($"======== running LogisticRegression.TrainAndEvaluate using {device.Type} ========");
+            LogisticRegression.TrainAndEvaluate(device);
+
+            Console.WriteLine($"======== running MNISTClassifier.TrainAndEvaluate with multilayer perceptron (MLP) classifier using {device.Type} ========");
+            MNISTClassi
