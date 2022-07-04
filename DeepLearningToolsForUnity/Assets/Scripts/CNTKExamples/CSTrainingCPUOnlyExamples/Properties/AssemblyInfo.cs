6fd8c02607aaf566552c5843a3d56509802907b2
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-07-04 14:18:51 +0000

    v0.10.2

--- DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/CSTrainingCPUOnlyExamples/Properties/AssemblyInfo.cs
+++ DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/CSTrainingCPUOnlyExamples/Properties/AssemblyInfo.cs
@@ -2,4 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Ge
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Image")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Image")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false
