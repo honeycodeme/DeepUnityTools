9ba7760382738812999c6959776ef16144e08fdb
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-07-08 08:27:11 +0000

    2rand[0,1,1]

--- DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/CSTrainingCPUOnlyExamples/Properties/AssemblyInfo.cs
+++ DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/CSTrainingCPUOnlyExamples/Properties/AssemblyInfo.cs
@@ -14,4 +14,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e34bcbf0-9e9b-4225-8c1a-7592c95bac0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      
