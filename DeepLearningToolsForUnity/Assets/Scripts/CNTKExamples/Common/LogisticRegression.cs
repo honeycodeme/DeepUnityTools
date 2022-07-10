1dc18cf0bc7fd98eafcc1304aa2e1638db830a25
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-07-10 16:24:05 +0000

    removed include

--- DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/Common/LogisticRegression.cs
+++ DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/Common/LogisticRegression.cs
@@ -4,4 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace
+namespace CNTK.CSTrainingExamples
+{
+    /// <summary>
+    /// This class shows how the train and evaluate a Logistic Regression classifier.
+    /// Data are randomly generated into 2 classes with statistically separable features.
+    /// See https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_101_LogisticRegression.ipynb for more details.
+    /// </summary>
+    public class LogisticRegression
+    {
+        static int inputDim = 3;
+        static int numOutputClasses = 2;
+
+        static public void TrainAndEvaluate(De
