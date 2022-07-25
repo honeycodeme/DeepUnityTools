875d6fe2d35a8ee46d21dac6ec6b05b4048ed750
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-07-25 23:06:12 +0000

    package json update

--- DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/Common/MNISTClassifier.cs
+++ DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/Common/MNISTClassifier.cs
@@ -7,4 +7,13 @@ namespace CNTK.CSTrainingExamples
     /// <summary>
     /// This class shows how to build and train a classifier for handwritting data (MNIST).
     /// For more details, please follow a serial of tutorials below:
-    /// https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_103A_MNIS
+    /// https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_103A_MNIST_DataLoader.ipynb
+    /// https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_103B_MNIST_LogisticRegression.ipynb
+    /// https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_103C_MNIST_MultiLayerPerceptron.ipynb
+    /// https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_103D_MNIST_ConvolutionalNeuralNetwork.ipynb
+    /// </summary>
+    public class MNISTClassifier
+    {
+        /// <summary>
+        /// Execution folder is: CNTK/x64/BuildFolder
+        /// Data folder is: CNTK/Tests/EndToEndTests/Imag
