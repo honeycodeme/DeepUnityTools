4591eb0d19ec36ecee2fd1df2c11fcaa31beda45
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-07-25 21:30:37 +0000

    [MERGE] merge from trunk addons

---
+++ DeepLearningToolsForUnity/Assets/Scripts/CNTKExamples/Common/MNISTClassifier.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CNTK.CSTrainingExamples
+{
+    /// <summary>
+    /// This class shows how to build and train a classifier for handwritting data (MNIST).
+    /// For more details, please follow a serial of tutorials below:
+    /// https://github.com/Microsoft/CNTK/blob/master/Tutorials/CNTK_103A_MNIS
