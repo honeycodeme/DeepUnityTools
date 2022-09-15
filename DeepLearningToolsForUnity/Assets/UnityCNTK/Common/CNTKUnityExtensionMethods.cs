e91ab2b0cc3a8046bf5803b3019d886e7fae4bfc
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-09-15 15:09:06 +0000

    Fixed everything

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Common/CNTKUnityExtensionMethods.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Common/CNTKUnityExtensionMethods.cs
@@ -1,1 +1,18 @@
-﻿using 
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CNTK;
+namespace UnityCNTK
+{
+    public static class CNTKUnityExtensionMethods
+    {
+        /// <summary>
+        /// Shallow copy a IList and retrn a array.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="iList"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        public static T[] CopyToArray<T>(this IList<T> iList, int startIndex = 0)
+        {
+            T[] result =
