3b33ab4d34541a23d4066f5cea5e1eb71235163f
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-09-23 17:46:44 +0000

    removed clutter

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Editor/UsefulLayouts.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Editor/UsefulLayouts.cs
@@ -11,4 +11,16 @@ namespace UnityCNTK.Editor
     {
 
         public Rect WindowRect { get; set; } = new Rect(50, 50, 100, 100);
-  
+        public Vector2 MinWindowSize { get; set; } = new Vector2(75, 50);
+        protected bool isResize = false;
+        public int ID { get; set; }
+
+        protected static GUIStyle styleWindowResize = null;
+        protected static GUIContent gcDrag = new GUIContent("", "drag to resize");
+        protected static Vector2 buttonSize = new Vector2(10, 10);
+        protected static float dragableTopHeight = 20;
+
+
+        public string Title { get; set; }
+
+        public ResizableSubWindow(int id, R
