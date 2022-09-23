b23072ef49737d6054d006f354007f0ff5a5cc7f
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-09-23 05:42:51 +0000

    clean up project

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Editor/UsefulLayouts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace UnityCNTK.Editor
+{
+
+    public class ResizableSubWindow
+    {
+
+        public Rect WindowRect { get; set; } = new Rect(50, 50, 100, 100);
+  
