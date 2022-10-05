65bb1a933afb06661df4b0be6d0150252bdbaac7
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-10-05 21:24:13 +0000

    Update release notes

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Trainers.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Trainers.cs
@@ -91,3 +91,110 @@ namespace UnityCNTK
             //train
             trainer.TrainMinibatch(inputDataMap, false, Device);*/
         }
+
+        public void TrainMiniBatch(float[] inputs, float[] targets)
+        {
+            var inputVar = networkRef.InputLayer.InputVariable;
+            var inputValue = Value.CreateBatch(inputVar.Shape, inputs, Device);
+
+            var targetVar = networkRef.OutputLayer.GetTargetInputVariable();
+            var targetValue = Value.CreateBatch(targetVar.Shape, targets, Device);
+
+            //var outputVar = networkRef.OutputLayer.GetOutputVariable();
+            //var lossVar = networkRef.OutputLayer.GetTrainingLossVariable();
+
+            var inputDataMap = new Dictionary<Variable, Value>() { { inputVar, inputValue }, { targetVar, targetValue } };
+            //var outputDataMap = new Dictionary<Variable, Value>() { { outputVar, null }, { lossVar, null } };
+
+            //train
+            trainer.TrainMinibatch(inputDataMap, false, Device);
+        }
+
+        public void SetLearningRate(float lr)
+        {
+            learners[0].SetLearningRateSchedule(new TrainingParameterScheduleDouble(lr));
+        }
+    }
+
+
+    /// <summary>
+    /// A class to hide CNTK learners api
+    /// </summary>
+    public static class LearnerDefs
+    {
+
+
+        public static SGDLearnerDef SGDLearner(float learningRate)
+        {
+            return new SGDLearnerDef(learningRate);
+        }
+        public static MomentumSGDLearnerDef MomentumSGDLearner(float learningRate, float momentum)
+        {
+            return new MomentumSGDLearnerDef(learningRate, momentum);
+        }
+        public static AdamLearnerDef AdamLearner(float learningRate = 0.001f, float beta1 = 0.9f, float beta2 = 0.999f, float epsilon = 0.00001f)
+        {
+            return new AdamLearnerDef(learningRate, beta1, beta2, epsilon);
+        }
+        public abstract class LearnerDef
+        {
+            /// <summary>
+            /// create CNTK learner
+            /// </summary>
+            /// <param name="parameters"></param>
+            /// <returns></returns>
+            public abstract Learner Create(IList<Parameter> parameters);
+        }
+
+        public class SGDLearnerDef : LearnerDef
+        {
+            private float lr;
+            public SGDLearnerDef(float learningRate)
+            {
+                lr = learningRate;
+            }
+            public override Learner Create(IList<Parameter> parameters)
+            {
+                return Learner.SGDLearner(parameters, new TrainingParameterScheduleDouble(lr));
+            }
+        }
+
+        public class MomentumSGDLearnerDef : LearnerDef
+        {
+            private float lr;
+            private float mom;
+            public MomentumSGDLearnerDef(float learningRate, float momentum)
+            {
+                lr = learningRate;
+                mom = momentum;
+            }
+            public override Learner Create(IList<Parameter> parameters)
+            {
+
+                return Learner.MomentumSGDLearner(parameters, new TrainingParameterScheduleDouble(lr), new TrainingParameterScheduleDouble(mom), true);
+            }
+        }
+
+        public class AdamLearnerDef : LearnerDef
+        {
+            private float lr;
+            private float b1;
+            private float b2;
+            private float eps;
+            public AdamLearnerDef(float learningRate = 0.001f, float beta1 = 0.9f, float beta2 = 0.999f, float epsilon = 0.00001f)
+            {
+                lr = learningRate;
+                b1 = beta1;
+                b2 = beta2;
+                eps = epsilon;
+            }
+            public override Learner Create(IList<Parameter> parameters)
+            {
+
+                return CNTKLib.AdamLearner(new ParameterVector(parameters.ToArray()), new TrainingParameterScheduleDouble(lr),
+                    new TrainingParameterScheduleDouble(b1), true, new TrainingParameterScheduleDouble(b2), eps);
+            }
+        }
+    }
+
+}
