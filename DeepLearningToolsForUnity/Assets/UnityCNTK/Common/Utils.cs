bcb958ee1872e9056bd744b6c7250fb50637468f
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-10-08 07:32:17 +0000

    fixing indentation

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Utils.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Common/Utils.cs
@@ -51,4 +51,25 @@ namespace UnityCNTK
 
         public bool JustUpdated
         {
-            get; private set
+            get; private set;
+        }
+
+        private float lastAverage = 0;
+        private int currentCount = 0;
+        private float sum = 0;
+
+        public AutoAverage(int interval = 1)
+        {
+            Interval = interval;
+            JustUpdated = false;
+        }
+
+        public void AddValue(float value)
+        {
+            sum += value;
+            currentCount += 1;
+            JustUpdated = false;
+            if (currentCount >= Interval)
+            {
+                lastAverage = sum / currentCount;
+                cu
