c27c7b1b1f89769449e4439495fd247c9d23a82d
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-10-19 07:50:05 +0000

    Publish page-11 index

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+using System;
+
+public class SerializableDictionaryPropertyDrawer : PropertyDrawer
+{
+	const string KeysFieldName = "m_keys";
+	const string ValuesFieldName = "m_values";
+
+	static GUIContent m_iconPlus = IconContent ("Toolbar Plus", "Add entry");
+	static GUIContent m_iconMinus = IconContent ("Toolbar Minus", "Rem
