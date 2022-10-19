aa280fe0989587f3eb1e1509cf823a4639c4fb41
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-10-19 09:30:53 +0000

    Added license notice

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawer.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/Editor/SerializableDictionaryPropertyDrawer.cs
@@ -11,4 +11,13 @@ public class SerializableDictionaryPropertyDrawer : PropertyDrawer
 	const string ValuesFieldName = "m_values";
 
 	static GUIContent m_iconPlus = IconContent ("Toolbar Plus", "Add entry");
-	static GUIContent m_iconMinus = IconContent ("Toolbar Minus", "Rem
+	static GUIContent m_iconMinus = IconContent ("Toolbar Minus", "Remove entry");
+	static GUIContent m_warningIconConflict = IconContent ("console.warnicon.sml", "Conflicting key, this entry will be lost");
+	static GUIContent m_warningIconOther = IconContent ("console.infoicon.sml", "Conflicting key");
+	static GUIContent m_warningIconNull = IconContent ("console.warnicon.sml", "Null key, this entry will be lost");
+	static GUIStyle m_buttonStyle = GUIStyle.none;
+
+	object m_conflictKey = null;
+	object m_conflictValue = null;
+	int m_conflictIndex = -1 ;
+	
