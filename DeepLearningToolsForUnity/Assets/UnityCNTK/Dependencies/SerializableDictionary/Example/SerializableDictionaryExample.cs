3964c24881d18068901755e585c832d373871c3f
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-12-16 11:01:21 +0000

    updated Readme.md

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/Example/SerializableDictionaryExample.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/Example/SerializableDictionaryExample.cs
@@ -2,4 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Seriali
+public class SerializableDictionaryExample : MonoBehaviour {
+	// The dictionaries can be accessed throught a property
+	[SerializeField]
+	StringStringDictionary m_stringStringDictionary;
+	public IDictionary<string, string> StringStringDictionary
+	{
+		get { return m_stringStringDictionary; }
+		set { m_stringStringDictionary.CopyFrom (value); }
+	}
+
+	public ObjectColorDictionary m_objectColorDictionary;
+
+	void Reset ()
+	{
+		// access by property
+		StringStringDictionary = new Dictionary<string, string>() { {"first key"
