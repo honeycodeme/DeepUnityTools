c7456467b49f1b2385cab532d51d0b6c89af903d
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-12-16 21:49:51 +0000

    Roll recipe dependencies (trivial).

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/SerializableDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+	[SerializeField]
+	TKey[] m_keys;
+	[SerializeField]
+	TValue[] m_values;
+
+	public SerializableDictionary()
+	{
+	}
+
+	public SerializableDictionary(IDictionary<TKey, TVal
