fffef7cdafa745ba0204eef24ad2af6dbf4c1939
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2022-12-18 15:44:04 +0000

    0.0.69

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/SerializableDictionary.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Dependencies/SerializableDictionary/SerializableDictionary.cs
@@ -14,4 +14,38 @@ public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IS
 	{
 	}
 
-	public SerializableDictionary(IDictionary<TKey, TVal
+	public SerializableDictionary(IDictionary<TKey, TValue> dict) : base(dict.Count)
+	{
+		foreach (var kvp in dict)
+		{
+			this[kvp.Key] = kvp.Value;
+		}
+	}
+
+	public void CopyFrom(IDictionary<TKey, TValue> dict)
+	{
+		this.Clear();
+		foreach (var kvp in dict)
+		{
+			this[kvp.Key] = kvp.Value;
+		}
+	}
+
+	public void OnAfterDeserialize()
+	{
+		if(m_keys != null && m_values != null && m_keys.Length == m_values.Length)
+		{
+			this.Clear();
+			int n = m_keys.Length;
+			for(int i = 0; i < n; ++i)
+			{
+				this[m_keys[i]] = m_values[i];
+			}
+
+			m_keys = null;
+			m_values = null;
+		}
+
+	}
+
+	public void OnBeforeSeri
