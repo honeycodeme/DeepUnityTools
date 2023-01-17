0be9dbcb94e736205b2cba1c252c6e18e7438cf1
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-01-17 05:42:22 +0000

    Updated Travis CI

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Examples/TestGANTrain2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityCNTK;
+using CNTK;
+
+public class TestGANTrain2D : MonoBehaviour {
+
+
+
+    protected GAN gan;
+    protected TrainerGAN trainerGan;
+    public DataPlane2D dataPlane;
+    public float lrGenerator = 0.001f;
+    public float lrDiscriminator = 0.001f;
+    public bool training = false;
+
+
+    public int trainedEpisodes = 0;
+
+    // Use this for initialization
+    void Start () {
+  
