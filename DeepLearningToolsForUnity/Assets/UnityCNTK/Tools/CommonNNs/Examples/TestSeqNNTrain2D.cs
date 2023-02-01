4fe2c88483ebfb3139eed03450851e43d53d356f
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-02-01 06:32:59 +0000

    ARM: 8427/1: dma-mapping: add support for offset parameter in dma_mmap()

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Examples/TestSeqNNTrain2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CNTK;
+using System.Linq;
+using UnityCNTK;
+using UnityCNTK.Tools.StyleAndTexture;
+using UnityCNTK.LayerDefinitions;
+using System.Threading;
+using System;
+using MathNet.Numerics;
+using Accord.Math;
+
+public class TestSeqNNTrain2D : MonoBehaviour {
+
+    protected SequentialNetworkDense network;
+    public DataPlane2D dataPlane;
+    protected TrainerSimpleNN trainer;
+    public float lr = 0.00001
