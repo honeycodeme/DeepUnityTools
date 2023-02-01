3f13a0678c0577d3d9c7f68cac2071f69361d5b2
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-02-01 15:33:23 +0000

    Bump version for release [minor]

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Examples/TestSeqNNTrain2D.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Examples/TestSeqNNTrain2D.cs
@@ -16,4 +16,31 @@ public class TestSeqNNTrain2D : MonoBehaviour {
     protected SequentialNetworkDense network;
     public DataPlane2D dataPlane;
     protected TrainerSimpleNN trainer;
-    public float lr = 0.00001
+    public float lr = 0.00001f;
+    public bool training = false;
+    protected OutputLayerDef outputLayer;
+    // Use this for initialization
+    void Start () {
+        CreateResnode();
+
+        //TestLayerNormalization();
+
+    }
+	
+
+	// Update is called once per frame
+	void Update () {
+        if (training)
+        {
+            TrainOnce(50);
+        }
+    }
+    
+    
+
+    public void CreateResnode()
+    {
+        var input = new InputLayerDense(2);
+
+        //outputLayer = new OutputLayerDenseBayesian(1);
+        outputLayer = 
