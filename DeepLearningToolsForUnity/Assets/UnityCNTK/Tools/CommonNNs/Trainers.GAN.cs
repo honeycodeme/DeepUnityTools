407562734c9c4d6a2470b15e51b586c684ac07cc
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-02-12 09:16:54 +0000

    fix e2e

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Trainers.GAN.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CNTK;
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityCNTK
+{
+    public class TrainerGAN
+    {
+        protected Trainer trainerG;
+        protected Trainer trainerD;
+
+        protected List<Learner> learnersG;
+       
