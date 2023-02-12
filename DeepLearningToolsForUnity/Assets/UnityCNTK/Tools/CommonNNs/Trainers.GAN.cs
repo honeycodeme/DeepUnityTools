6df80bc7a0cde93378e292d7dd0c493dcb32994a
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-02-12 14:18:48 +0000

    bump version to 0.0.6

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Trainers.GAN.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Trainers.GAN.cs
@@ -26,4 +26,17 @@ namespace UnityCNTK
         public float LastLossDiscriminator { get { return (float)trainerD.PreviousMinibatchLossAverage(); } }
 
         public float LearningRateGenerator { get; private set; }
-        public float LearningRateD
+        public float LearningRateDiscriminator { get; private set; }
+
+        protected Dictionary<int,CNTKDictionary> savedLearners;
+        protected Dictionary<int, Dictionary<Parameter, NDArrayView>> savedParameters;
+
+        public bool usePredictionInTraining = false;
+
+        public TrainerGAN(GAN gan, LearnerDefs.LearnerDef generatorLearner, LearnerDefs.LearnerDef discriminatorLearner, DeviceDescriptor device, int maxDataBufferCount = 50000)
+        {
+
+            Device = device;
+            ganReference = gan;
+
+            learnersG = new
