6ea97bb4c9bbf8e82b9fc6629248ca6b6cc7f60b
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-02-12 10:28:14 +0000

    pypi

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Trainers.GAN.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/CommonNNs/Trainers.GAN.cs
@@ -13,4 +13,17 @@ namespace UnityCNTK
         protected Trainer trainerD;
 
         protected List<Learner> learnersG;
-       
+        protected List<Learner> learnersD;
+
+        protected GAN ganReference;
+        
+        public DeviceDescriptor Device { get; set; }
+
+        protected DataBuffer dataBuffer;
+
+
+        public float LastLossGenerator { get { return (float)trainerG.PreviousMinibatchLossAverage(); } }
+        public float LastLossDiscriminator { get { return (float)trainerD.PreviousMinibatchLossAverage(); } }
+
+        public float LearningRateGenerator { get; private set; }
+        public float LearningRateD
