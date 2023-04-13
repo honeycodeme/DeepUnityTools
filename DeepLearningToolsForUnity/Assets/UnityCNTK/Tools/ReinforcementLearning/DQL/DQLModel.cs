2f913befc7ce1b41d1ae79e4d9913ba78a5d772c
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-04-13 08:07:27 +0000

    corrected docs

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/DQL/DQLModel.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/DQL/DQLModel.cs
@@ -1,1 +1,21 @@
-﻿using Sy
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using CNTK;
+using UnityCNTK.LayerDefinitions;
+using MathNet.Numerics.Distributions;
+
+namespace UnityCNTK.ReinforcementLearning
+{
+    public class DQLModel
+    {
+
+        public QNetwork Network { get; protected set; }
+        public int StateSize { get { return Network.StateSize; } }
+        public int ActionSize { get { return Network.ActionSize; } }
+        public DeviceDescriptor Device { get { return Network.Device; } }
+
+
+        //---variables for
