bb46138152c123fb29ffbf85ca0647092ac8f736
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-04-17 12:15:51 +0000

    Makefile updated.

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/DQL/DQLModel.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/DQL/DQLModel.cs
@@ -18,4 +18,15 @@ namespace UnityCNTK.ReinforcementLearning
         public DeviceDescriptor Device { get { return Network.Device; } }
 
 
-        //---variables for
+        //---variables for training
+        public Variable InputOldAction { get; protected set; }
+        public Variable InputState { get { return Network.InputState; } }
+        public Variable InputTargetQ { get; protected set; }
+        
+        public Variable OutputLoss { get; protected set; }
+        public Variable OutputAction { get; protected set; }
+        public Variable OutputQs { get { return Network.OutputQs; } }
+        public Variable OutputMaxQ { get; protected set; }
+
+        //test
+        Variable outputTargetQ;
