48da8bf8e2983b941f9abe0206d2f1c77bf52a5f
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-06-28 16:53:01 +0000

    Bump to next development iteration

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/3DBall/Scripts/Ball3DRunner.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/3DBall/Scripts/Ball3DRunner.cs
@@ -8,4 +8,23 @@ using CNTK;
 using UnityEngine.UI;
 using System.IO;
 
-public class Ball3DRunner 
+public class Ball3DRunner : MonoBehaviour {
+    public Text scoreUI;
+    public Ball3DEnviroment environment;
+    public string saveDataPath;
+    public float learningRate = 0.001f;
+    protected PPOModel model;
+    protected TrainerPPOSimple trainer;
+
+    public int episodesThisTrain = 0;
+    public int trainedCount = 0;
+
+    public int episodeToRunForEachTrain = 30;
+    public int iterationForEachTrain = 50;
+    public int minibatch = 32;
+
+    [Range(0,100)]
+    public float timeScale;
+    public bool training = true;
+
+    protect
