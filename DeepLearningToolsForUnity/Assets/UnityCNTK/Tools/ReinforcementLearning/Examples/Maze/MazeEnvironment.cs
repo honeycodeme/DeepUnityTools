e38f0da015c9709e629ef3264d471b6f7534c4bd
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-07-29 11:52:20 +0000

    add gif

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Maze/MazeEnvironment.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Maze/MazeEnvironment.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 using UnityCNTK;
 using UnityCNTK.ReinforcementLearning;
 
-public class MazeEnvironment: Mono
+public class MazeEnvironment: MonoBehaviour,IRLEnvironment
+{
+   
+    public Vector2i mazeDimension;
+    public bool regenerateMapOnReset = false;
+    public bool randomWallChance = false;
+    public float wallChanceOnNonPath = 0.3f;
+    public int maxStepAllowed = 20;
+    public float failureReward = -100;
+    public float maxWinReward = 100;
+    public float goToWallReward;
+    public float goUpReward = 1;
+    public float goCloserReward = 1;
+    public float stepCostReward = -0.1f;
+    
+    private Vector2i startPosition;
+    private Vector2i goalPosition;
+    
