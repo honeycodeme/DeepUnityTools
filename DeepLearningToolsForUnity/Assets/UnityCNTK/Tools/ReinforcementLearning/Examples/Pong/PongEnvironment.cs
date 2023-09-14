d12dc77dc57b8925f7d170827c4501ecd44d9d30
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-09-14 08:22:54 +0000

    fint

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Pong/PongEnvironment.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Pong/PongEnvironment.cs
@@ -8,4 +8,21 @@ using UnityCNTK.ReinforcementLearning;
 
 public class PongEnvironment : MonoBehaviour,IRLEnvironment {
 
-    public ControlSource playerLeftC
+    public ControlSource playerLeftControl;
+    public ControlSource playerRightControl; 
+
+    public float failureReward = -1;
+    public float winReward = 1;
+    public float hitBallReward = 0.1f;
+
+    public float racketSpeed = 0.02f;
+    public float ballSpeed = 0.01f;
+    public float racketWidth = 0.05f;
+
+    public readonly int ActionUp = 2;
+    public readonly int ActionDown = 0;
+    public readonly int ActionStay = 1;
+
+    public float leftStartX = -1;
+    public float rightStartX = 1;
+    public Vector2 arenaSize = new Vector2
