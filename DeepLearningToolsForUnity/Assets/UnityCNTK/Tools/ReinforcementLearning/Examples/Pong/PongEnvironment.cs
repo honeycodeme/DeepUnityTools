fffafa12280571b6f0dabcaec44155297c1da533
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-09-14 05:55:29 +0000

    Update database

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Pong/PongEnvironment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityCNTK;
+using CNTK;
+using UnityCNTK.ReinforcementLearning;
+
+
+public class PongEnvironment : MonoBehaviour,IRLEnvironment {
+
+    public ControlSource playerLeftC
