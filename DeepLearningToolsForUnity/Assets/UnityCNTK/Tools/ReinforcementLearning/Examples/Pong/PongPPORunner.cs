b6c80a1e93b1b627d242fa94bdbcbda9e0fecb27
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-10-03 18:00:03 +0000

    updated changelog.

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Pong/PongPPORunner.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Pong/PongPPORunner.cs
@@ -2,4 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using Unit
+using UnityCNTK.ReinforcementLearning;
+using UnityCNTK;
+using CNTK;
+using UnityEngine.UI;
+using System.IO;
+
+public class PongPPORunner : MonoBehaviour {
+    public PongEnvironment environment;
+    public string saveDataPath = "PongPPO.bytes";
+    public float learningRate = 0.001f;
+    protected PPOModel model;
+    protected TrainerPPOSimple trainer;
+
+    public int episodesThisTrain = 0;
+    public int trainedCount = 0;
+
+    public int episodeToRunForEachTrain = 30;
+    public int iterationForEachTrain = 50;
+    public int
