e289a4ee5b832c2e2a6901e9421cb68df9e65d44
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-11-21 12:04:58 +0000

    Upgrade gradle wrapper

---
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/Examples/Pong/ShowInfoPong.cs
@@ -0,0 +1,37 @@
+
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowInfoPong : MonoBehaviour {
+    public PongDQLRunner DQLrunner;
+    public PongPPORunner PPOrunner;
+    public PongEnvironment environment;
+    public Text stepsText;
+    public Text episodeText;
+    public Text leftWinText;
+    public Text rightWinText;
+    public Text leftWinPerText;
+    public Text leftHitPercText;
+    public Text infoText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (DQLrunner && DQLrunner.isActiveAndEnabled)
+        {
+            stepsText.text = "Total Steps: " + DQLrunner.Steps.ToString();
+            episodeText.text = "Total Episodes: " + DQLrunner.currentEpisode.ToString();
+            leftWinText.text = DQLrunner.leftWin.ToString();
+            rightWinText.text = DQLrunner.rightWin.ToString();
+            leftWinPerText.text = "Win Rate: " + (DQLrunner.winningRate50Left.Average).ToString();
+            leftHitPercText.text = "Ave Reward: " + DQLrunner.reward50EpiLeft.Average.ToString();
+            infoText.gameObject.SetActive(false);
+        }
+        else if (PPOrunner && PPOrunner.isActiveAndEnabled)
+        {
+            stepsText.text = "Total Steps: " + PPOrunner.Steps.ToString();
