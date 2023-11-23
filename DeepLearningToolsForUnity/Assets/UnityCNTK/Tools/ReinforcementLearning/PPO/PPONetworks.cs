0372a90678c37560524b3a03c6ee7b80231f41fe
honeycodeme <144710600+honeycodeme@users.noreply.github.com>
2023-11-23 20:59:22 +0000

    Same

--- DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/PPO/PPONetworks.cs
+++ DeepLearningToolsForUnity/Assets/UnityCNTK/Tools/ReinforcementLearning/PPO/PPONetworks.cs
@@ -7,4 +7,16 @@ using CNTK;
 using UnityCNTK.LayerDefinitions;
 using MathNet.Numerics.Distributions;
 
-namespace UnityCNTK.Reinforce
+namespace UnityCNTK.ReinforcementLearning
+{
+    public abstract class PPONetwork
+    {
+        public abstract int StateSize { get; protected set; }
+        public abstract int ActionSize { get; protected set; }
+
+        public abstract bool IsActionContinuous { get; protected set; }
+        public abstract Variable InputState { get; protected set; }
+
+        public abstract Variable OutputMean { get; protected set; }
+        public abstract Variable OutputVariance { get; protected set; }
+        public abstr
